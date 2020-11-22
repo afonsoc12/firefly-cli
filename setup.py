1009180517c0c7da9b558d96d8b0d7f487cb4d77
Afonso Costa <afonsoc1997@gmail.com>
2020-11-22 14:43:52 +0000

    Fixed typo in setup.py dependencies

--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="firefly-cli",
-    version="0.0.2",
+    version="0.0.3",
     author="Afonso Costa",
-    #author_email="author@example.com",
     description="A python-based command line interface for practically entering expenses in Firefly III",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -16,7 +15,7 @@ setuptools.setup(
     install_requires=[
         'pandas==1.1.4',
         'requests==2.25.0',
-        'tabulate-0.8.7'
+        'tabulate==0.8.7'
     ],
     entry_points = {
         'console_scripts': [
