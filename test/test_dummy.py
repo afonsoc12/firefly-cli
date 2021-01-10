82f91f076d9d0e94a9e7d14224367c6f296a3682
Afonso Costa <afonsoc1997@gmail.com>
2021-01-10 20:13:53 +0000

    Dummy tests added for CI

---
+++ test/test_dummy.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestCase(unittest.TestCase):
+
+    def test_dummy(self):
+        self.assertEqual('tests to be added', 'tests to be added')
+
+
+if __name__ == '__main__':
+    unittest.main()
