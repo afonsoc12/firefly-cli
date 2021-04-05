bfbda44c0d9e5ec423739366f3581028762ad21b
Afonso Costa <afonsoc1997@gmail.com>
2021-04-05 13:04:48 +0100

    Version changed to pep440-pre Updated .gitignore

--- firefly_cli/_version.py
+++ firefly_cli/_version.py
@@ -40,7 +40,7 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440"
+    cfg.style = "pep440-pre"
     cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "firefly-cli-v"
     cfg.versionfile_source = "firefly_cli/_version.py"
