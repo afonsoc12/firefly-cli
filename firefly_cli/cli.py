136bd78f790c8918a302b74b37e71e70a782f9bb
Afonso Costa <afonsoc1997@gmail.com>
2022-02-27 14:00:26 +0000

    Refactored firefly-cli with cmd2. Now supports adding many transaction arguments using argparse. Fixes #6

--- firefly_cli/cli.py
+++ firefly_cli/cli.py
@@ -1,79 +1,77 @@
 import json
-import traceback
 from datetime import date
-from cmd import Cmd
 
+import cmd2
 from tabulate import tabulate
-from .utils import parse_transaction_to_df
+
 from ._version import get_versions
-from .configs import *
 from .api import FireflyAPI
+from .configs import *
+from .parsers import get_add_parser
+from .transaction import Transaction
+from .utils import prompt_continue
 
 
+def help_exit():
+    print("exit the application. Shorthand: x q Ctrl-D.")
 
-class FireflyPrompt(Cmd):
-    prompt = '🐷 ➜ '
+
+class FireflyPrompt(cmd2.Cmd):
+    prompt = "🐷 ➜ "
+    cmd2.Cmd.set_window_title("🐷 firefly-cli")
     configs = load_configs()
-    api = FireflyAPI(configs['API'].get('URL'),
-                     configs['API'].get('API_TOKEN'))
+    api = FireflyAPI(configs["API"].get("URL"), configs["API"].get("API_TOKEN"))
 
-    is_url_set = True if configs['API'].get('URL') is not None else False
-    is_api_token_set = True if configs['API'].get('API_TOKEN') is not None else False
+    is_url_set = True if configs["API"].get("URL") is not None else False
+    is_api_token_set = True if configs["API"].get("API_TOKEN") is not None else False
 
     # Display information to update URL or API token, if not already set
-    opt_text = ''
+    opt_text = ""
     if not (is_url_set and is_api_token_set):
         if is_url_set and not is_api_token_set:
-            opt_text += 'It appears that you have not set your API token yet.\nType \"edit API_TOKEN <TOKEN>\" to do so.'
+            opt_text += '\n[WARNING] It appears that you have not set your API token yet.\nType "edit API_TOKEN <TOKEN>" to do so.\n'
         elif not is_url_set and is_api_token_set:
-            opt_text += 'It appears that you have not set your URL yet.\nType \"edit URL <URL>\" to do so.'
+            opt_text += '\n[WARNING] It appears that you have not set your URL yet.\nType "edit URL <URL>" to do so.\n'
         elif not (is_url_set and is_api_token_set):
-            opt_text += 'It appears that you have not set neither of your URL or API token yet.\nType \"edit URL <URL>\" or \"edit API_TOKEN <TOKEN>\" to do so.'
-    #else:
-    #    opt_text += '\n'
+            opt_text += '\n[WARNING] It appears that you have not set neither your URL nor API token yet.\nType "edit URL <URL>" or "edit API_TOKEN <TOKEN>" to do so.\n'
 
-
-    intro = '''
-Copyright {} Afonso Costa
+    intro = f"""
+Copyright {date.today().year} Afonso Costa
 
 Licensed under the Apache License, Version 2.0 (the "License");
 Type \"license\" for more information.
-
 
 Welcome to FireflyIII Command Line Interface!
 Created by Afonso Costa (@afonsoc12)
 
 ===== Status =====
-  - URL: {}
-  - API Token: {}
-  - Connection: {}
-  - Version: {}
-{}
+  - URL: {configs["API"]["URL"] if is_url_set else "(not set)"}
+  - API Token: {"*****" + configs["API"]["API_TOKEN"][-5:] if is_api_token_set else "(not set)"}
+  - Connection: {"OK!" if api.api_test else "No connection!"}
+  - Version: {get_versions()["version"]}
+==================
+{opt_text}
 Type \"help\" to list commands.
-    '''.format(date.today().year,
-               configs['API']['URL'] if is_url_set else '(not set)',
-               '*****' + configs['API']['API_TOKEN'][-5:] if is_api_token_set else '(not set)',
-               'OK!' if api.api_test else 'No connection!',
-               get_versions()['version'],
-               opt_text)
-
+"""
 
     @classmethod
     def refresh_api(cls):
         cls.configs = load_configs()
-        cls.api = FireflyAPI(cls.configs['API']['URL'], cls.configs['API']['API_TOKEN'])
-        print('API refreshed. Current Status: {}'.format('OK!' if cls.api.api_test else 'No connection!'))
+        cls.api = FireflyAPI(cls.configs["API"]["URL"], cls.configs["API"]["API_TOKEN"])
+        print(
+            "API refreshed. Current Status: {}".format(
+                "OK!" if cls.api.api_test else "No connection!"
+            )
+        )
 
-    def do_exit(self, input):
-        print('Bye! Come store new transactions soon!')
+    def do_exit(self, _):
+        print("Bye! Come store new transactions soon!")
         return True
 
-    def help_exit(self):
-        print('exit the application. Shorthand: x q Ctrl-D.')
-
-    def do_license(self, input):
-        print('''
-Copyright 2020 Afonso Costa
+    def do_license(self, _):
+        print(
+            f"""
+Copyright {date.today().year} Afonso Costa
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -86,115 +84,104 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-''')
+"""
+        )
+
+    def do_version(self, _):
+        print(f"firefly-cli: v{get_versions()['version']}")
+
+    def help_version(self):
+        print("Displays version information.")
 
     def help_license(self):
-        print('Displays License information')
+        print("Displays License information.")
 
-    def do_refresh(self, input):
+    def do_refresh(self, _):
         FireflyPrompt.refresh_api()
 
     def help_refresh(self):
-        print('Refreshes API connection')
+        print("Refreshes API connection.")
 
-    def do_edit(self, input):
-        input_split = input.split(' ')
+    def do_edit(self, args):
+        input_split = args.split(" ")
         if len(input_split) != 2:
-            print(f'The command \"edit\" takes exactly two arguments. Provided: {input}')
+            print(f'The command "edit" takes exactly two arguments. Provided: {args}')
         else:
-            if input_split[0] == 'URL' or input_split[0] == 'API_TOKEN':
-                self.configs['API'][input_split[0]] = input_split[1]
+            if input_split[0] == "URL" or input_split[0] == "API_TOKEN":
+                self.configs["API"][input_split[0]] = input_split[1]
                 save_configs_to_file(self.configs)
                 FireflyPrompt.refresh_api()
             else:
-                print(f'The argument \"{input_split[0]}\" is not recognised.')
-
-    def complete_edit(self, text, line, begidx, endidx):
-        if not text:
-            completions = self.configs['API'].keys()
-        else:
-            completions = [f
-                           for f in self.configs['API'].keys()
-                           if f.startswith(text)
-                           ]
-        return completions
+                print(f'The argument "{input_split[0]}" is not recognised.')
 
     def help_edit(self):
-        print('Shows your available budgets.')
+        print(
+            "Edits connection credentials:\n\t> edit URL http://<FireflyIII URL>:<Port>\n\t> edit API <API key>"
+        )
 
-    def do_accounts(self, input):
+    def do_accounts(self, args):
         # TODO: Implement with pagination based
         accounts = self.api.get_accounts()
         print(json.dumps(accounts, sort_keys=True, indent=4))
 
     def help_accounts(self):
-        print('Shows your accounts.')
+        print("Shows your accounts.")
 
-    def do_budgets(self, input):
+    def do_budgets(self, args):
         # TODO: Implement with pagination based
         budgets = self.api.get_budgets()
         print(json.dumps(budgets, sort_keys=True, indent=4))
 
     def help_budgets(self):
-        print('Shows your budgets.')
+        print("Shows your budgets.")
 
-    def do_add(self, input):
-        try:
-            data = parse_transaction_to_df(input)
-            response = self.api.create_transaction(data)
-            if response.status_code == 200:
-                print(f'Transaction {input} successfully added!')
-                print('Interpreted as:')
-                print(tabulate(data, headers='keys', tablefmt='psql'))
-            elif response.status_code == 422:
-                print(f'The data provided is not valid! Transaction: {input}')
-                print('Interpreted as:')
-                print(tabulate(data, headers='keys', tablefmt='psql'))
-                msg = response.json()['message']
-                errors = response.json()['errors']
-                print(f'\nMessage: {msg}')
-                print('Errors:')
-                for e in errors:
-                    e_line = '; '.join(errors[e])
-                    print(f'\t- {e}: {e_line}')
+    @cmd2.with_argparser(get_add_parser())
+    def do_add(self, parser):
+        trans = Transaction.from_input(parser)
+        trans.parse_inline_transaction_to_attributes()
 
+        missing_fields = trans.mandatory_fields_missing()
+        if missing_fields:
+            raise ValueError(
+                f"Your transaction does not have these mandatory fields: {', '.join(missing_fields)}"
+            )
 
-        except Exception:
-            print(f'An error has occurred.')
-            traceback.print_exc()
+        tab_header, tab_body = trans.get_tabulates()
+        print(f"Transaction header:\n{tab_header}\n")
+        print(f"Transaction Body:\n{tab_body}\n")
 
-    def help_add(self):
-        print('''
-Adds a transaction to Firefly in a comma-separated fashion.
+        if prompt_continue(extra_line=False, extra_msg=" adding the transaction"):
+            try:
+                response = self.api.create_transaction(trans)
 
-Fields are: 
-{}
-The three first fields can't be omitted.
+                if response.status_code == 200:
+                    print(f"Transaction successfully added!")
+                elif response.status_code == 422:
+                    print(f"The data provided is not valid!")
+                    msg = response.json()["message"]
+                    errors = response.json()["errors"]
+                    print(f"\nMessage: {msg}")
+                    print("Errors:")
+                    for e in errors:
+                        e_line = "; ".join(errors[e])
+                        print(f"\t- {e}: {e_line}")
 
-Examples:
-    - A simple one:
-        -> `5, Large Mocha, Cash`
-    - One with all the fields being used:
-        -> `5, Large Mocha, Cash, Starbucks, Coffee Category, Food Budget`
-    - You can skip specfic fields by leaving them empty (except the first two):
-        -> `5, Large Mocha, Cash, , , UCO Bank`
-'''.format(tabulate([['Amount', 'Description', 'Source account', 'Destination account', 'Category', 'Budget']], tablefmt='psql')))
+            except Exception:
+                print(f"An error has occurred.")
+                raise
 
+    def default(self, args):
+        if args.lower() in (
+            "x",
+            "q",
+        ):
+            return self.do_exit(args)
 
-    def default(self, input):
-        if input == 'x' or input == 'q':
-            return self.do_exit(input)
-
-        print('{} not recognized. Please type \"help\" to list the available commands'.format(input))
+        print(
+            '{} not recognized. Please type "help" to list the available commands'.format(
+                args
+            )
+        )
 
     do_EOF = do_exit
     help_EOF = help_exit
-
-
-#if __name__ == '__main__':
-#    import sys
-#
-#    if len(sys.argv) > 1:
-#        FireflyPrompt().onecmd(' '.join(sys.argv[1:]))
-#    else:
-#        FireflyPrompt().cmdloop()
--- firefly_cli/configs.py
+++ firefly_cli/configs.py
@@ -1,21 +1,27 @@
-from pathlib import Path
 import configparser
+import os
+from pathlib import Path
+
 from xdg.BaseDirectory import xdg_config_home
 
-config_file_path = Path(xdg_config_home).joinpath('firefly-cli', 'firefly-cli.ini')
+if os.getenv("FIREFLY_CLI_CONFIG"):
+    config_file_path = Path(os.environ["FIREFLY_CLI_CONFIG"])
+else:
+    config_file_path = Path(xdg_config_home).joinpath("firefly-cli", "firefly-cli.ini")
 
-# Create dir if not exists
-config_file_path.parents[0].mkdir(parents=True, exist_ok=True)
+    # Create dir if not exists
+    config_file_path.parent.mkdir(parents=True, exist_ok=True)
+
 
 def load_configs():
     configs = configparser.ConfigParser()
 
     # No config file loaded because it was not available/not existent
     if len(configs.read(config_file_path)) < 1:
-        print('File not found, creating the file..')
+        print("File not found, creating the file..")
 
-        with open(config_file_path, 'w') as f:
-            configs['API'] = {}
+        with open(config_file_path, "w") as f:
+            configs["API"] = {}
             save_configs_to_file(configs)
 
     return configs
@@ -23,9 +29,12 @@ def load_configs():
 
 def save_configs_to_file(configs):
     try:
-        with open(config_file_path, 'w') as f:
+        with open(config_file_path, "w") as f:
             configs.write(f)
-            print('Config file saved at {}'.format(str(config_file_path)))
+            print("Config file saved at {}".format(str(config_file_path)))
     except:
-        print('An error has occurred while saving file to {}'.format(str(config_file_path)))
-
+        print(
+            "An error has occurred while saving file to {}".format(
+                str(config_file_path)
+            )
+        )
