536637d669de81793046edcfa43ca8934e50f916
Afonso Costa <afonsoc1997@gmail.com>
2023-04-08 15:59:07 +0100

    Fixes #14

--- firefly_cli/configs.py
+++ firefly_cli/configs.py
@@ -4,23 +4,29 @@ from pathlib import Path
 
 from xdg.BaseDirectory import xdg_config_home
 
-if os.getenv("FIREFLY_CLI_CONFIG"):
-    config_file_path = Path(os.environ["FIREFLY_CLI_CONFIG"])
-else:
-    config_file_path = Path(xdg_config_home).joinpath("firefly-cli", "firefly-cli.ini")
 
-    # Create dir if not exists
-    config_file_path.parent.mkdir(parents=True, exist_ok=True)
+def config_file_path():
+    if os.getenv("FIREFLY_CLI_CONFIG"):
+        config_file_path = Path(os.environ["FIREFLY_CLI_CONFIG"])
+    else:
+        config_file_path = Path(xdg_config_home).joinpath(
+            "firefly-cli", "firefly-cli.ini"
+        )
+
+        # Create dir if not exists
+        config_file_path.parent.mkdir(parents=True, exist_ok=True)
+
+    return config_file_path
 
 
 def load_configs():
     configs = configparser.ConfigParser()
 
     # No config file loaded because it was not available/not existent
-    if len(configs.read(config_file_path)) < 1:
+    if len(configs.read(config_file_path())) < 1:
         print("File not found, creating the file..")
 
-        with open(config_file_path, "w") as f:
+        with open(config_file_path(), "w") as f:
             configs["firefly-cli"] = {}
             save_configs_to_file(configs)
 
@@ -29,12 +35,12 @@ def load_configs():
 
 def save_configs_to_file(configs):
     try:
-        with open(config_file_path, "w") as f:
+        with open(config_file_path(), "w") as f:
             configs.write(f)
-            print("Config file saved at {}".format(str(config_file_path)))
+            print("Config file saved at {}".format(str(config_file_path())))
     except:
         print(
             "An error has occurred while saving file to {}".format(
-                str(config_file_path)
+                str(config_file_path())
             )
         )
